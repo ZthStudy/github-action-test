ad45ad8d85aaeaa01d46c6b046983d071d9589a8
TianHou Zhang <814331049@qq.com>
2022-08-26 13:28:38 +0800

    build lib

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-// import { createApp } from 'vue';
+import { createApp } from 'vue';
 // import App from './App.vue';
 
 // import App from './Test';
-
-// createApp(App).mount('#app');
 
 import TEST from './Test';
 
 export function add(num1: number, num2: number): number {
   return num1 + num2;
 }
+
+createApp(TEST).mount('#app');
 
 export default TEST;
